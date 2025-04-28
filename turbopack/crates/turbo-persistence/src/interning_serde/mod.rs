f30bb9a989c847ec96054665ae2e5d29db16427b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-28 15:03:41 -0700

    Wrap!

--- turbopack/crates/turbo-persistence/src/interning_serde/mod.rs
+++ turbopack/crates/turbo-persistence/src/interning_serde/mod.rs
@@ -1,3 +1,28 @@
 //! Exposed for usage in `turbo-tasks-backend`
 
-scoped_tls!();
+use indexmap::IndexSet;
+use rustc_hash::FxBuildHasher;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use turbo_rcstr::RcStr;
+
+#[derive(Serialize, Deserialize)]
+struct Data(Vec<u8>, IndexSet<RcStr, FxBuildHasher>);
+
+pub fn to_vec<T>(value: &T) -> pot::Result<Vec<u8>>
+where
+    T: Serialize,
+{
+    let (result, ser_map) = turbo_rcstr::set_ser_map(|| pot::to_vec(value));
+    let value = result?;
+    let data = Data(value, ser_map);
+    pot::to_vec(&data)
+}
+
+pub fn from_slice<T>(slice: &[u8]) -> pot::Result<T>
+where
+    T: DeserializeOwned,
+{
+    let data: Data = pot::from_slice(slice)?;
+
+    turbo_rcstr::set_de_map(&data.1, || pot::from_slice(&data.0))
+}
