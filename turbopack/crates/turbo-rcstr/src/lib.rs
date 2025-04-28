7fe9a3ea6661a14d129df4b086674c0102a12654
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-28 14:59:50 -0700

    Optimize serde of rcstr

--- turbopack/crates/turbo-rcstr/src/lib.rs
+++ turbopack/crates/turbo-rcstr/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{
     borrow::{Borrow, Cow},
+    cell::RefCell,
     ffi::OsStr,
     fmt::{Debug, Display},
     hash::{Hash, Hasher},
@@ -10,6 +11,8 @@ use std::{
 };
 
 use debug_unreachable::debug_unreachable;
+use indexmap::IndexSet;
+use rustc_hash::FxBuildHasher;
 use scoped_tls::scoped_thread_local;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use shrink_to_fit::ShrinkToFit;
@@ -128,6 +131,20 @@ impl RcStr {
         }
 
         Self { unsafe_data: alias }
+    }
+
+    fn ref_count(&self) -> usize {
+        match self.tag() {
+            DYNAMIC_TAG => {
+                // convert `self` into `arc`
+                let arc = unsafe { dynamic::restore_arc(self.unsafe_data) };
+                let count = Arc::count(&arc);
+                forget(arc);
+                count
+            }
+            INLINE_TAG => 1,
+            _ => unsafe { debug_unreachable!() },
+        }
     }
 }
 
@@ -285,24 +302,70 @@ scoped_thread_local!(
     /// Map of strings to their interned ids.
     ///
     /// This is used to serialize strings to their interned ids.
-    static SER_MAP: RefCell<FxHashMap<RcStr, u64>>
+    static SER_MAP: RefCell<IndexSet<RcStr,FxBuildHasher>>
 );
 
 scoped_thread_local!(
     /// Read-only map of strings to their interned ids
-    static DE_MAP: FxHashMap<RcStr, u64>
+    static DE_MAP: IndexSet<RcStr,FxBuildHasher>
 );
+
+pub fn set_ser_map<F, R>(f: F) -> (R, IndexSet<RcStr, FxBuildHasher>)
+where
+    F: FnOnce() -> R,
+{
+    let map = Default::default();
+
+    let r = { SER_MAP.set(&map, f) };
+
+    (r, map.into_inner())
+}
+
+pub fn set_de_map<F, R>(map: &IndexSet<RcStr, FxBuildHasher>, f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    DE_MAP.set(map, f)
+}
 
 impl Serialize for RcStr {
     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        if SER_MAP.is_set() && self.len() >= 3 && (self.len() < 512 || self.ref_count() >= 2) {
+            return SER_MAP.with(|ser| {
+                if let Some(id) = ser.borrow().get_index_of(self) {
+                    serializer.serialize_u32(id as u32)
+                } else {
+                    let id = ser.borrow_mut().insert(self.clone());
+                    serializer.serialize_u32(id as u32)
+                }
+            });
+        }
+
         serializer.serialize_str(self.as_str())
     }
 }
 
 impl<'de> Deserialize<'de> for RcStr {
     fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let s = String::deserialize(deserializer)?;
-        Ok(RcStr::from(s))
+        use serde::de::Error;
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum Repr {
+            String(String),
+            Id(u32),
+        }
+
+        let repr = Repr::deserialize(deserializer)?;
+
+        match repr {
+            Repr::String(s) => Ok(RcStr::from(s)),
+            Repr::Id(id) => DE_MAP.with(|map| {
+                let s = map
+                    .get_index(id as usize)
+                    .ok_or_else(|| D::Error::custom(format!("failed to find id: {}", id)))?;
+                Ok(s.clone())
+            }),
+        }
     }
 }
 
