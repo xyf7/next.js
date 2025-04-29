52eea3cb82897ba28e21ed604f248be0f97928ec
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-29 09:24:33 -0700

    `intern_for_serialize`

--- turbopack/crates/turbo-rcstr/src/lib.rs
+++ turbopack/crates/turbo-rcstr/src/lib.rs
@@ -328,19 +328,35 @@ where
     DE_MAP.set(map, f)
 }
 
+/// Intern a string for serialization.
+///
+/// This function exists to move the logic for accessing the SER_MAP out of the hot path of
+/// `Serialize::serialize`.
+#[inline(never)]
+fn intern_for_serialize(str: &RcStr) -> Option<u32> {
+    if !SER_MAP.is_set() {
+        return None;
+    }
+
+    Some(SER_MAP.with(|ser| {
+        let mut borrow = ser.borrow_mut();
+        if let Some(id) = borrow.get_index_of(str) {
+            id as u32
+        } else {
+            let id = borrow.len();
+            borrow.insert(str.clone());
+            id as u32
+        }
+    }))
+}
+
 impl Serialize for RcStr {
     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        if SER_MAP.is_set() && self.len() >= 3 && (self.len() < 512 || self.ref_count() >= 2) {
-            return SER_MAP.with(|ser| {
-                let mut borrow = ser.borrow_mut();
-                if let Some(id) = borrow.get_index_of(self) {
-                    serializer.serialize_u32(id as u32)
-                } else {
-                    let id = borrow.len();
-                    borrow.insert(self.clone());
-                    serializer.serialize_u32(id as u32)
-                }
-            });
+        if self.len() >= 3 && (self.len() < 512 || self.ref_count() >= 2) {
+            let id = intern_for_serialize(self);
+            if let Some(id) = id {
+                return serializer.serialize_u32(id);
+            }
         }
 
         serializer.serialize_str(self.as_str())
